5c1917d6b379ce087364351ae3cd18e4b0ec1a4c
Brian Adams <brian@brianadams.codes>
2017-01-19 15:58:48 -0500

    Get SciPy installed and running.

--- homework/hw1.py
+++ homework/hw1.py
@@ -1,6 +1,19 @@
-import matplotlib.pyplot as plot
+from sklearn import datasets
+from sklearn.model_selection import cross_val_predict
+from sklearn import linear_model
+import matplotlib.pyplot as plt
 
-xs = [2, 3, 5, 7, 11]
-ys = [4, 9, 5, 9, 1]
-plot.plot(xs, ys)
-plot.savefig("squaremod10.png")
+lr = linear_model.LinearRegression()
+boston = datasets.load_boston()
+y = boston.target
+
+# cross_val_predict returns an array of the same size as `y` where each entry
+# is a prediction obtained by cross validation:
+predicted = cross_val_predict(lr, boston.data, y, cv=10)
+
+fig, ax = plt.subplots()
+ax.scatter(y, predicted)
+ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+ax.set_xlabel('Measured')
+ax.set_ylabel('Predicted')
+plt.show()
